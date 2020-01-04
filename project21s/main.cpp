51b94e2a8c7a54d3e4e92265d6b4e080cc4c5e81
ovenCode <ovenCode@users.noreply.github.com>
2020-01-04 18:44:09 +0100

    Project21s 1st Launch attempt

--- project21s/main.cpp
+++ project21s/main.cpp
@@ -3,29 +3,32 @@
 
 using namespace std;
 
-void odpowiedz(vector <int>);
+void odpowiedz(vector<int>);
 
 int main()
 {
     int n = 0, m = 0, wierzI = 0, wierzJ = 0;
-    vector <int> graf;
+    vector<int> graf;
     cin >> n >> m;
-    for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++)
     {
-        cin >> wierzI >> wierzJ;
-        graf.push_back(wierzI);
-        graf.push_back(wierzJ);
+        for (int i = 0; i < m; i++)
+        {
+            cin >> wierzI >> wierzJ;
+            graf.push_back(wierzI);
+            graf.push_back(wierzJ);
+        }
     }
-    
     odpowiedz(graf);
 
     return 0;
 }
 
-void odpowiedz(vector <int> graf)
+void odpowiedz(vector<int> graf)
 {
 
     int a = graf[0], b = graf[1], temp = 0, temp2 = 0;
+    vector<int> wierzcholki, wynik;
 
     for (int i = 2; i < graf.size(); i + 2)
     {
@@ -35,13 +38,47 @@ void odpowiedz(vector <int> graf)
             a = graf[i];
             graf[i] = temp;
             temp2 = b;
-            b = graf[i-1];
-            graf[i-1] = temp2;
+            b = graf[i - 1];
+            graf[i - 1] = temp2;
         }
+    }
+
+    for (int i = 0; i < graf.size(); i++)
+    {
+        if (graf[i] != graf[i + 1])
+        {
+            wierzcholki.push_back(graf[i]);
+        }
+        if (graf[i + 1] == graf.size())
+            continue;
+    }
+
+    temp = wierzcholki.size();
+
+    for (int i = 0; i < graf.size(); i+2)
+    {
+        for (int j = 0; j < wierzcholki.size(); j++)
+        {
+            if (((graf[i] == wierzcholki[i]) && (graf[i-1] == wierzcholki[i-1])) || ((graf[i] == wierzcholki[j-1]) && (graf[i-1] == wierzcholki[j])))
+            {
+                wynik.push_back(graf[i]);
+                wynik.push_back(graf[i-1]);
+            }
+            
+        }
+        
         
     }
     
 
-    cout << "1: " << endl;
+    cout << "1: ";
+    for (int i = 0; i < wynik.size(); i+2)
+    {
+        cout << wynik[i] << " ";
+    }
     cout << "2: " << endl;
+    for (int i = 1; i < wynik.size(); i+2)
+    {
+        cout << wynik[i] << " ";
+    }
 }
