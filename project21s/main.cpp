3fa681534be2bca4f67d423b317f197a493ccfaa
ovenCode <ovenCode@users.noreply.github.com>
2020-01-07 23:45:56 +0100

    Project21s 2nd Launch attempt

--- project21s/main.cpp
+++ project21s/main.cpp
@@ -24,7 +24,7 @@ int main()
     {
         cout << graf[i] << endl;
     } */
-    
+
     //t(graf);
 
     odpowiedz(graf);
@@ -35,48 +35,70 @@ int main()
 void odpowiedz(vector<int> graf)
 {
 
-    int a = graf[0], b = graf[1], temp = 0, temp2 = 0;
-    vector<int> wierzcholki, wynik;
+    int a = graf[0], b = graf[1], temp = 0, temp2 = 0, maks = 0;
+    vector<int> wierzcholki, wynik, tempy;
 
     for (int i = 0; i < graf.size() - 2; i += 2)
     {
         if (a < graf[i])
         {
+            maks = graf[i];
             temp = a;
             a = graf[i];
             graf[i] = temp;
+            if(maks < graf[i+1]) maks = graf[i+1];
             temp2 = b;
-            b = graf[i+1];
-            graf[i+1] = temp2;
+            b = graf[i + 1];
+            graf[i + 1] = temp2;
         }
     }
 
-    for (int i = 0; i < graf.size() - 1; i++)
+    for (int i = 0; i < maks; i++)
     {
-        if (graf[i] != graf[i + 1])
-        {
-            wierzcholki.push_back(graf[i]);
-        }
+        tempy.push_back(0);
+    }
     
-    }
-
-    temp = wierzcholki.size();
-
-    for (int i = 0; i < graf.size() - 2; i+=2)
+    for (int i = 0; i < graf.size(); i++)
     {
-        for (int j = 0; j < wierzcholki.size() - 1; j++)
+        if (temp != graf[i])
         {
-            if (((graf[i] == wierzcholki[j]) && (graf[i+1] == wierzcholki[j+1])) || ((graf[i] == wierzcholki[j+1]) && (graf[i+1] == wierzcholki[j])))
+            if (i==0)
             {
-                wynik.push_back(graf[i]);
-                wynik.push_back(graf[i+1]);
+                wierzcholki.push_back(graf[i]);
+                temp = graf[i];
+            }
+            cout << "Inside for 1: " << i << endl;
+            for (int j = 0; j < wierzcholki.size(); j++)
+            {
+                cout << "Inside for 2: " << j << endl;
+                if(graf[i] != wierzcholki[j])
+                {
+                    tempy[graf[i] - 1]++;
+                }
             }
             
         }
         
-        
     }
-    
+
+    for (int i = 0; i < wierzcholki.size(); i++)
+    {
+        cout << wierzcholki[i] << " ";
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < graf.size() - 2; i += 2)
+    {
+        for (int j = 0; j < wierzcholki.size() - 1; j++)
+        {
+            if (((graf[i] == wierzcholki[j]) && (graf[i + 1] == wierzcholki[j + 1])) || ((graf[i] == wierzcholki[j + 1]) && (graf[i + 1] == wierzcholki[j])))
+            {
+                wynik.push_back(graf[i]);
+                wynik.push_back(graf[i + 1]);
+            }
+        }
+    }
 
     cout << "1: ";
     for (int i = 0; i < wynik.size(); i++)
@@ -84,20 +106,20 @@ void odpowiedz(vector<int> graf)
         cout << wynik[i] << " ";
     }
     cout << "2: " << wynik.size();
-     /*for (int i = 1; i < wynik.size() - 1; i+=2)
+    /*for (int i = 1; i < wynik.size() - 1; i+=2)
         {
             cout << wynik[i] << " ";
         } */
-    }
+}
 
-    void t(vector<int> graf)
-    {
-        int a = graf[0], b = graf[1], c = 0;
+void t(vector<int> graf)
+{
+    int a = graf[0], b = graf[1], c = 0;
 
-        cout << a << " " << b << " " << c << endl;
-        vector<int> sam;
+    cout << a << " " << b << " " << c << endl;
+    vector<int> sam;
 
-        cin >> a;
-        sam.push_back(a);
-        cout << sam[0];
-    }
+    cin >> a;
+    sam.push_back(a);
+    cout << sam[0];
+}
