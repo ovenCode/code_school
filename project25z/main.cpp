b34cdd6f492c6c083153aa09abb84a8e23fbfffa
ovenCode <ovenCode@users.noreply.github.com>
2020-01-21 07:19:54 +0100

    Project25z Initialization

---
+++ project25z/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// FUNCTION DECLARATION
+
+
+
+//
+
+int main()
+{
+
+// MAIN VARIABLES INITIALIZATION
+
+int T = 0, P = 0, C = 0, M = 0;
+
+//
+
+
+
+return 0;
+
+}
+
+// FUNCTION DEFINITION
+
+
+
+//
