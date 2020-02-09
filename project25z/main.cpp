68432837ccb1e3215a8edbd499be8b28be8ab036
ovenCode <ovenCode@users.noreply.github.com>
2020-02-09 01:23:18 +0100

    Project25z Project Idea

--- project25z/main.cpp
+++ project25z/main.cpp
@@ -14,10 +14,53 @@ int main()
 
 // MAIN VARIABLES INITIALIZATION
 
-int T = 0, P = 0, C = 0, M = 0;
+int T = 0, P = 0, C = 0, M = 0, B = 0, W = 0; // T - test cases, P - number of pins, C - number of colours, M - number of already played guesses, B - black points, W - white points
+int * S = 0, * G = 0, * H = 0;
 
 //
 
+cin >> T;
+
+for (int i = 0; i < T; i++)
+{
+    cin >> P >> C >> M;
+    S = new int [P];
+    G = new int [P];
+    H = new int [2*P];
+
+    for (int j = 0; j < M; j++)
+    {
+
+        for (int k = 0; k < P; k++)
+        {
+            cin >> G[k];
+        }
+        cin >> B >> W;
+
+        if (W > 0)
+        {
+
+            for (int k = 0; k < P; k++)
+            {
+                S[k] = G[k];
+            }
+            
+            if (B == P)
+            {
+                
+                for (int k = 0; k < P; k++)
+                {
+                    cout << G[k] << " ";
+                }
+                
+            }
+            
+        }
+        
+        
+    }
+    
+}
 
 
 return 0;
@@ -29,3 +72,15 @@ return 0;
 
 
 //
+
+/*
+
+POTENTIAL SOLUTION
+
+cin >> T >> P >> C >> M;
+
+M:
+0 1 2 ... P
+B W
+
+*/
