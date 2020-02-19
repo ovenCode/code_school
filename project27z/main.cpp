777c5893eeefcb9f96e47c0989a5469c128c332b
ovenCode <ovenCode@users.noreply.github.com>
2020-02-19 20:56:33 +0100

    Project28z Initialization

---
+++ project27z/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// FUNCTION DECLARATION
+
+
+
+//
+
+int main()
+{
+
+// MAIN VARIABLES INITIALIZATION
+
+int t = 0, n = 0;
+string s = "";
+
+//
+
+cin >> t;
+
+for(int i = 0; i < t; i++)
+{
+    cin >> n;
+    for(int j = 0; j < n; j++)
+    {
+        
+    }
+}
+
+return 0;
+
+}
+
+// FUNCTION DEFINITION
+
+
+
+//
