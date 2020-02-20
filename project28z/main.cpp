cae323906cf409e72445a57dc33e345a27cfc0c2
ovenCode <ovenCode@users.noreply.github.com>
2020-02-20 07:35:11 +0100

    Project28z Small update

--- project28z/main.cpp
+++ project28z/main.cpp
@@ -14,8 +14,8 @@ int main()
 
 // MAIN VARIABLES INITIALIZATION
 
-int t = 0, n = 0;
-string s = "";
+int t = 0, n = 0, x = 0, y = 0, r = 0, z = 0;
+char s = ' ';
 
 //
 
@@ -26,7 +26,19 @@ for(int i = 0; i < t; i++)
     cin >> n;
     for(int j = 0; j < n; j++)
     {
-        
+        cin >> s;
+        switch (s)
+        {
+        case 'p':
+            cin >> x >> y;
+            break;
+        case 'c':
+        break;
+        case 'l':
+        break;
+        default:
+            break;
+        }
     }
 }
 
