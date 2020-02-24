0151f0c82e10c3a936579b09c959b6e210faa1d0
ovenCode <ovenCode@users.noreply.github.com>
2020-02-24 22:30:29 +0100

    Project28z Small update

--- project28z/main.cpp
+++ project28z/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 // FUNCTION DECLARATION
 
-
+void mbr(vector<int>, char);
 
 //
 
@@ -16,6 +17,7 @@ int main()
 
 int t = 0, n = 0, x = 0, y = 0, r = 0, z = 0;
 char s = ' ';
+vector<int> box;
 
 //
 
@@ -31,16 +33,30 @@ for(int i = 0; i < t; i++)
         {
         case 'p':
             cin >> x >> y;
+            box.push_back(x);
+            box.push_back(y);
             break;
         case 'c':
+                cin >> x >> y >> r;
+                box.push_back(x);
+                box.push_back(y);
+                box.push_back(r);
         break;
         case 'l':
+                cin >> x >> y >> r >> z;
+                box.push_back(x);
+                box.push_back(y);
+                box.push_back(r);
+                box.push_back(z);
         break;
         default:
             break;
         }
+        mbr(box,s);
     }
 }
+
+
 
 return 0;
 
@@ -48,6 +64,28 @@ return 0;
 
 // FUNCTION DEFINITION
 
+void mbr(vector <int> a, char c)
+{
+    int left = 0, right = 0, top = 0, bottom = 0;
+    if(c == 'p')
+    {
+        for (int i = 0; i < a.size(); i+2)
+        {
+            if(a[i] < left)
+            {
+                left = a[i];
+            }
+        }
+        
+    }
+    else if(c == 'c')
+    {
 
+    }
+    else if(c == 'l')
+    {
+
+    }
+}
 
 //
