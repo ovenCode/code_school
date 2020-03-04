12849a839494dcc821276814be3a2fa56fe0a75d
ovenCode <ovenCode@users.noreply.github.com>
2020-03-05 00:03:11 +0100

    Project32z Adding some basics

--- project32z/main.cs
+++ project32z/main.cs
@@ -1,3 +1,7 @@
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Data;
+using Internal;
 using System;
 
 namespace HiddenPassword
@@ -6,12 +10,37 @@ namespace HiddenPassword
     {
         static void Main()
         {
-            int t = 0;
+            int t = 0, n = 0, pos = 0;
             string s = "";
-
+            List <string> box = new List<string> ();
             t = Convert.toInt32(Console.ReadLine());
 
-            
+            for (int i = 0; i < t; i++)
+            {
+                n = Convert.toInt32(Console.ReadLine());
+                s = Console.ReadLine();
+                pos = s.indexOf(" ");
+                for (int j = 0; j < n; j++)
+                {
+                    box.Add(s.Substring());
+                }
+                foreach (string a in box)
+                {
+                    foreach (char c in a)
+                    {
+                        
+                    }            
+                }
+                }
+        }
+
+        public static string ConvToBin(char c)
+        {
+            int temp = 0;
+            string s = "";
+            temp = c;
+            s += temp.ToString();
+            return s;
         }
     }
 }
