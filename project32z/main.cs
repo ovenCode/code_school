7bc196e8659024110004ded8295eadb4c6bc5dfe
ovenCode <ovenCode@users.noreply.github.com>
2020-03-04 22:14:35 +0100

    Project32z Initialization

---
+++ project32z/main.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HiddenPassword
+{
+    class Program
+    {
+        static void Main()
+        {
+            int t = 0;
+            string s = "";
+
+            t = Convert.toInt32(Console.ReadLine());
+
+            
+        }
+    }
+}
